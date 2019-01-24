26bd8a69017a3f2b46e8829a465e3d37a2265313
Carine LE CHARLES <carine.le.charles@gmail.com>
2019-01-24 23:19:56 +0100

    updated skip before action for user not log in

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -1,5 +1,5 @@
 class BlogpostsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!
 
   def index
     @blogposts = Blogpost.all
