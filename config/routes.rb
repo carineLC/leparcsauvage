bac03d63d334ea7ed9b5459c190c92a845fa7d0c
Carine LE CHARLES <carine.le.charles@gmail.com>
2019-02-03 15:48:46 +0100

    added schedules, prices ans access routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,12 @@ Rails.application.routes.draw do
   get 'fauconnerie', to: 'pages#falconry', as: :falconry
   get 'parc_de_vision', to: 'pages#vision_park', as: :vision_park
   get 'centre_de_sauvegarde', to: 'pages#backup_center', as: :backup_center
+
   get 'equipe', to: 'pages#team', as: :team
   get 'partenariats', to: 'pages#partnerships', as: :partnerships
-  get 'infos_pratiques', to: 'pages#practical_infos', as: :practical_infos
   get 'mentions_legales', to: 'pages#legal_notices', as: :legal_notices
+
+  get 'horaires', to: 'pages#schedules', as: :schedules
+  get 'tarifs', to: 'pages#prices', as: :prices
+  get 'acces', to: 'pages#access', as: :access
 end
