3fcef5c4e1cfe765aa01ce4cb45bd0bba562868a
Carine LE CHARLES <carine.le.charles@gmail.com>
2019-02-07 21:03:36 +0100

    added js for mapbox

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,21 @@
+import mapboxgl from 'mapbox-gl';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+  if (mapElement) {
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [0.426193, 45.359098],
+      attributionControl: false,
+      zoom: 7
+    });
+    map.addControl(new mapboxgl.NavigationControl());
+    new mapboxgl.Marker({ color: '#74b49b' })
+        .setLngLat([0.426193, 45.359098])
+        .addTo(map);
+  }
+};
+
+export { initMapbox };
