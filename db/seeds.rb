f5ff185d4db389d5b7e1576b9e30e0251e4f8e7f
Carine LE CHARLES <carine.le.charles@gmail.com>
2019-02-08 17:08:41 +0100

    added admin user in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+User.create(email: "contactflyprod@gmail.com", password: "alexadmin", name: "Alexandre", admin: true)
