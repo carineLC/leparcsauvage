ecca010cf54323b81c44e54dcd8d49ce39c74286
claire gautier <claire.gautier@edhec.com>
2020-01-08 20:49:22 +0000

    Corrected indentation and removed commented lines

--- app/admin/workers.rb
+++ app/admin/workers.rb
@@ -33,17 +33,4 @@ ActiveAdmin.register Worker do
       row(:photo) { |w| image_tag(w.photo.url) }
     end
   end
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
 end
